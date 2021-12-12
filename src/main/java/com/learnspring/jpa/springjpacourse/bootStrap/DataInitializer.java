18e1c37662251a8cfc7c834daa59b427ceb8257e
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-12-12 21:05:02 +0530

    feat: Enable sql logging

---
+++ src/main/java/com/learnspring/jpa/springjpacourse/bootStrap/DataInitializer.java
@@ -0,0 +1,30 @@
+package com.learnspring.jpa.springjpacourse.bootStrap;
+
+import com.learnspring.jpa.springjpacourse.domain.Book;
+import com.learnspring.jpa.springjpacourse.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
+
+    public DataInitializer(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<Book> books = List.of(new Book("Spring Framework", "1234", "John"), new Book("Spring in Action", "1234", "James"));
+        bookRepository.saveAll(books);
+
+
+        // retrieve books
+
+        bookRepository.findAll().forEach(book -> System.out.println(book.getTitle()));
+    }
+}

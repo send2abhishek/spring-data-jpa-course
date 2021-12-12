d816db36b3808fa155ebbe598c1c115b8bb3c32a
Abhishek Kumar <abhishek160.kumar@ril.com>
2021-12-12 20:08:11 +0530

    first commit

---
+++ src/main/java/com/learnspring/jpa/springjpacourse/SpringJpaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.jpa.springjpacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnspring/jpa/springjpacourse/SpringJpaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.jpa.springjpacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
